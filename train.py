fa57b6504ca2e4d43df18d8685349e67e7b6a14a
ronen-halevy <ronen567@gmail.com>
2022-05-09 00:29:13 +0300

    dataset preprocess wip

--- train.py
+++ train.py
@@ -3,108 +3,129 @@
 # ================================================================
 #   Copyright (C) 2022 . All rights reserved.
 #
-#   File name   : train.py
-#   Author      : ronen halevy 
-#   Created date:  4/27/22
+#   File name   : scratch.py
+#   Author      : ronen halevy
+#   Created date:  5/2/22
 #   Description :
 #
 # ================================================================
-import tensorflow as tf
 import numpy as np
-# from matplotlib import pyplot as plt
+import tensorflow as tf
 import argparse
-from read_dataset import read_dataset
 
+from parse_tfrecords import parse_tfrecords
 
-# from ...create-tfrecords.render_dataset import render_dataset_examples
+from preprocess_dataset import preprocess_dataset
 
-def render(dataset):
-    data = dataset.take(1)
-    image, y = next(iter(data))
-    images = tf.expand_dims(image, axis=0)
+from utils import render
+from utils import generate_random_dataset
 
-    image_size = 416
-    boxes = y[..., 0:4].numpy().astype(float) / [image.shape[1], image.shape[0], image.shape[1], image.shape[0]]
-    indices = [1, 0, 3, 2]
-    boxes = boxes[..., indices]
-    boxes = boxes[0:10]
-    boxes = tf.expand_dims(boxes, axis=0)
-    boxes = tf.cast(boxes, float)
-    # image_pil = Image.fromarray(np.uint8(image.numpy() * 255))
-    # annotated_bbox_image = draw_bounding_box(image_pil, y[..., 0:4], color=(255, 255, 0),
-    #
-    #                                        thickness=1)
-    colors = [[1, 1, 1]]
-
-    # bb = np.take_along_axis(boxes, range(9), axis=1)
-
-    images = tf.image.draw_bounding_boxes(
-        images, boxes, colors, name=None
-    )
-
-    from matplotlib import pyplot as plt
-
-    plt.imshow(images.numpy()[0])
-    plt.grid()
-    plt.show()
-
-
-def image_preporcessn(image, y, t_w, t_h):
-    s_h, s_w, _ = image.shape
-
-    scale = min(t_w / s_w, t_h / s_h)
-
-    n_w, n_h = int(scale * s_w), int(scale * s_h)
-    boxes = (y[..., 0:4] * scale) + [(t_w - n_w) // 2, (t_h - n_h) // 2, (t_w - n_w) // 2, (t_h - n_h) // 2]
-    classes = y[..., 4]
-
-    classes = classes[tf.newaxis, :, tf.newaxis]
-
-    boxes = tf.expand_dims(boxes, axis=0)
-
-    y_n = tf.concat([boxes, classes], axis=-1)
-
-    scaled_image = tf.image.pad_to_bounding_box(
-        image, (t_h - n_h) // 2, (t_w - n_w) // 2, t_h, t_w
-    )
-    y_n = tf.squeeze(y_n, axis=0)
-    return scaled_image, y_n
 
 
 def main():
     parser = argparse.ArgumentParser()
+
+    parser.add_argument('--use_debug_dataset', default=False, action='store_true')
+
+    parser.add_argument('--render_dataset_example', default=False, action='store_true')
 
     parser.add_argument("--tfrecords_dir", type=str,
                         default='/home/ronen/PycharmProjects/create-tfrecords/dataset/tfrecords',
                         help='path to tfrecords files')
     parser.add_argument("--limit", type=int, default=None,
                         help='limit on max input examples')
+
     parser.add_argument("--classes", type=str,
                         default='/home/ronen/PycharmProjects/shapes-dataset/dataset/class.names',
                         help='path to classes names file needed to annotate plotted objects')
+
     parser.add_argument("--max_boxes", type=int, default=100,
                         help='max bounding boxes in an example image')
 
-    parser.add_argument("--t_w", type=int, default=416,
-                        help='target image width')
+    parser.add_argument("--batch_size", type=int, default=32,
+                        help='batch_size')
 
-    parser.add_argument("--t_h", type=int, default=516,
-                        help='target image height')
+    parser.add_argument("--image_size", type=int, default=416,
+                        help='Algorithm"s image_size . Assumed a square')
+
+    parser.add_argument("--anchors_file", type=str, default=None,
+                        help='anchors_file')
+
 
     args = parser.parse_args()
-
     tfrecords_dir = args.tfrecords_dir
-    t_w = args.t_w
-    t_h = args.t_h
-
+    image_size = args.image_size
+    batch_size = args.batch_size
     class_file = args.classes
     max_boxes = args.max_boxes
-    dataset = read_dataset(class_file, tfrecords_dir, max_boxes)
-    # tf.config.run_functions_eagerly(True)
+    use_debug_dataset = args.use_debug_dataset
+    anchors_file = args.anchors_file
 
-    dataset = dataset.map(lambda x, y: (image_preporcessn(x, y, t_w, t_h)))
+    if use_debug_dataset:
+        dataset = generate_random_dataset()
+        anchors = np.array(
+            [[[10, 13], [16, 30], [33, 23]], [[30, 61], [62, 45], [59, 119]], [[116, 90], [156, 198], [373, 326]]],
+            np.float32) / image_size
+    else:
+        dataset = parse_tfrecords(tfrecords_dir, image_size, max_boxes, class_file)
 
-    render(dataset)
+        # anchors =
+        # read_anchors(anchors_file) if anchors_file else \
+        anchors = np.array(
+            [[[10, 13], [16, 30], [33, 23]], [[30, 61], [62, 45], [59, 119]], [[116, 90], [156, 198], [373, 326]]],
+            np.float32) / image_size
+
+    # dataset_size = len(list(dataset))
+    dataset_size = 100 # ronen debug
+
+    train_size = int(0.7 * dataset_size)
+    val_size = int(0.20 * dataset_size)
+    test_size = int(0.10 * dataset_size)
+
+    dataset = dataset.shuffle(buffer_size=512, reshuffle_each_iteration=None)
+
+    train_dataset = dataset.take(train_size)
+    test_dataset = dataset.skip(train_size)
+    val_dataset = test_dataset.skip(test_size)
+    test_dataset = test_dataset.take(test_size)
+
+    if args.render_dataset_example:
+
+        render(train_dataset)
+
+    ###############################
+    image_size = 416
+    # downsize_strides = [32, 16, 8]
+    grid_sizes = np.array([13, 26, 52])
+
+    # anchors = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    #
+    batch_size = 32
+
+    from preprocess_dataset import preprocess_dataset
+    dataset = preprocess_dataset(dataset, batch_size, image_size, anchors, grid_sizes, max_boxes)
+    pass
+    # render(dataset)
+    pass
+
+    # scale_dataset = arrange_in_grid(y_train, anchors, image_size)
+    # print(scale_dataset.size())
+    # tensors = [scale_dataset.read(i) for i in range(scale_dataset.size().numpy())]
+    # print(tensors)
+
+    # downsize_strides = [32, 16, 8]
+    # grid_sizes = [13, 26, 52]
+    # anchors = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    #
+    # batch_size = 32
+    # image_size = 416
+
+
+
+    # train_dataset = preprocess_dataset(train_dataset, batch_size, image_size, anchors)
+    # test_dataset = preprocess_dataset(test_dataset, batch_size, image_size, anchors)
+    # val_dataset = preprocess_dataset(val_dataset, batch_size, image_size, anchors)
+    #
 
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+# coding=utf-8
+# ================================================================
+#   Copyright (C) 2022 . All rights reserved.
+#
+#   File name   : utils.py
+#   Author      : ronen halevy 
+#   Created date:  5/3/22
+#   Description :
+#
+# ================================================================
+
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+
+def generate_random_dataset(dataset_size=100, image_h=416, image_w=416, max_boxes=50, classes=80):
+    '''
+
+    :param dataset_size:
+    :type dataset_size:
+    :param image_h:
+    :type image_h:
+    :param image_w:
+    :type image_w:
+    :param max_boxes:
+    :type max_boxes:
+    :param classes:
+    :type classes:
+    :return:
+    :rtype:
+    '''
+    images = tf.zeros([dataset_size, image_h, image_w, 3])
+    xy_min = tf.random.uniform(shape=[dataset_size, max_boxes, 2], minval=0., maxval=0.8, dtype=tf.float32, seed=42)
+    x_max = tf.random.uniform(shape=[dataset_size, max_boxes, 1], minval=xy_min[:, :, 0:1], maxval=1, dtype=tf.float32,
+                              seed=42)
+    y_max = tf.random.uniform(shape=[dataset_size, max_boxes, 1], minval=xy_min[:, :, 1:2], maxval=1, dtype=tf.float32,
+                              seed=42)
+    boxes = tf.concat([xy_min, x_max, y_max], axis=-1) * image_h
+    classes = tf.random.uniform(shape=[dataset_size, max_boxes, 1], minval=0, maxval=classes, dtype=tf.int32, seed=42)
+    classes = tf.cast(classes, tf.float32)
+    labels = tf.concat([boxes, classes], -1)
+    dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+    return dataset
+
+
+def render(dataset):
+    '''
+
+    :param dataset: images and bounding box dataset
+    :type dataset:
+    :return: -
+    :rtype:
+    '''
+    image, y = list(dataset.as_numpy_iterator())[0]
+    images = tf.expand_dims(image, axis=0)# * 255
+
+    boxes = y[..., 0:4].astype(float)  # / [image.shape[1], image.shape[0], image.shape[1], image.shape[0]]
+    indices = [1, 0, 3, 2]
+    boxes = boxes[..., indices]
+    boxes = boxes[0:10]
+    boxes = tf.expand_dims(boxes, axis=0)
+    boxes = tf.cast(boxes, tf.float32) / 416
+
+    colors = [[1, 255, 0]]
+
+    images = tf.image.draw_bounding_boxes(
+        images, boxes, colors, name=None
+    )
+
+    plt.imshow(images.numpy()[0])
+    plt.show()
