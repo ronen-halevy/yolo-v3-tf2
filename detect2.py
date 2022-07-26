295a1faad1ebb7a431d943fb280c2d27acc7d64b
ronen-halevy <ronen567@gmail.com>
2022-07-27 01:09:42 +0300

    arrange classes

--- detect2.py
+++ detect2.py
@@ -12,22 +12,9 @@ from load_tfrecords import parse_tfrecords
 
 
 class Inference:
-    def __init__(self, output_dir, print_detections, save_result_images, display_result_images):
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-        try:
-            self.detections_outfile = f'{output_dir}/detect.txt'
-            os.remove(self.detections_outfile)
-        except OSError:
-            pass
-        self.detections_list_outfile = open(self.detections_outfile, 'a')
-        self.output_dir = output_dir
-        self.print_detections = print_detections
-        self.save_result_images = save_result_images
-        self.display_result_images = display_result_images
 
     @staticmethod
-    def do_inference(yolo, img_raw, size, class_names, yolo_max_boxes, bbox_color, font_size, index):
+    def _do_detection(yolo, img_raw, size, class_names, yolo_max_boxes, bbox_color, font_size, index):
         img = tf.expand_dims(img_raw, 0)
         img = resize_image(img, size, size)
         boxes, scores, classes, nums = yolo(img, training=False)
@@ -42,96 +29,107 @@ class Inference:
 
         return image_pil, image_detections_result
 
-    def output_detections_text(self, image_detections_result):
-        if self.print_detections:
+    @staticmethod
+    def _dump_detections_text(image_detections_result, print_detections, detections_list_outfile):
+        if print_detections:
             print(image_detections_result)
 
-        self.detections_list_outfile.write(f'{image_detections_result}\n')
-        self.detections_list_outfile.flush()
+        detections_list_outfile.write(f'{image_detections_result}\n')
+        detections_list_outfile.flush()
 
-    def output_annotated_image(self, annotated_image, out_filename):
-        outfile_path = f'{self.output_dir}/{out_filename}'
-        if self.save_result_images:
+    @staticmethod
+    def _output_annotated_image(annotated_image, output_dir, out_filename, save_result_images, display_result_images):
+        outfile_path = f'{output_dir}/{out_filename}'
+        if save_result_images:
             annotated_image.save(outfile_path)
-        if self.display_result_images:
+        if display_result_images:
             plt.imshow(annotated_image)
             plt.show()
 
+    def __call__(self,
+                 classes,
+                 anchors_file,
+                 weights,
+                 size,
+                 input_data_source,
+                 images_dir,
+                 tfrecords_dir,
+                 image_file_path,
+                 output_dir,
+                 yolo_max_boxes,
+                 nms_iou_threshold,
+                 nms_score_threshold,
+                 display_result_images,
+                 print_detections,
+                 save_result_images,
+                 bbox_color,
+                 font_size
+                 ):
 
-def detect(
-        classes,
-        anchors_file,
-        weights,
-        size,
-        input_data_source,
-        images_dir,
-        tfrecords_dir,
-        image_file_path,
-        output_dir,
-        yolo_max_boxes,
-        nms_iou_threshold,
-        nms_score_threshold,
-        display_result_images,
-        print_detections,
-        save_result_images,
-        bbox_color,
-        font_size
-):
-    anchors_table = get_anchors(anchors_file)
-    class_names = [c.strip() for c in open(classes).readlines()]
-    nclasses = len(class_names)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        detections_outfile = f'{output_dir}/detect.txt'
+        try:
+            os.remove(detections_outfile)
+        except OSError as e:
+            print(e)
+            exit(1)
 
-    yolov3_model = YoloV3Model()
+        detections_list_outfile = open(detections_outfile, 'a')
 
-    model = yolov3_model(size, nclasses, training=False, anchors_table=anchors_table,
-                        yolo_max_boxes=yolo_max_boxes, nms_iou_threshold=nms_iou_threshold,
-                        nms_score_threshold=nms_score_threshold)
+        anchors_table = get_anchors(anchors_file)
+        class_names = [c.strip() for c in open(classes).readlines()]
+        nclasses = len(class_names)
 
-    model.load_weights(weights).expect_partial()
-    print('weights loaded')
+        yolov3_model = YoloV3Model()
 
-    inferecnce = Inference(output_dir, print_detections, save_result_images, display_result_images)
+        model = yolov3_model(size, nclasses, training=False, anchors_table=anchors_table,
+                             yolo_max_boxes=yolo_max_boxes, nms_iou_threshold=nms_iou_threshold,
+                             nms_score_threshold=nms_score_threshold)
 
-    if input_data_source == 'tfrecords':
-        dataset = parse_tfrecords(tfrecords_dir, image_size=size, max_bboxes=yolo_max_boxes, class_file=None)
-        for index, entry in enumerate(dataset):
-            annotated_image, image_detections_result = inferecnce.do_inference(model, entry[0], size, class_names,
-                                                                               yolo_max_boxes, font_size,
-                                                                               index)
-            inferecnce.output_detections_text(image_detections_result)
-            inferecnce.output_annotated_image(annotated_image, out_filename=f'detect_{index}.jpg')
+        model.load_weights(weights).expect_partial()
+        print('weights loaded')
 
-    else:
-        if input_data_source == 'image_file':
-            filenames = [image_file_path]
-        elif input_data_source == 'images_dir':
-            # dirname = images_dir
-
-            import glob
-            types = ('*.jpeg', '*.jpg', '*.png', '*.bmp')  # the tuple of file types
-            filenames = []
-            for files in types:
-                filenames.extend(glob.glob(files))
-
-            from os.path import join
-            from glob import glob
-
-            files = []
-            for ext in ('*.jpeg', '*.jpg', '*.png', '*.bmp'
-                        ):
-                filenames.extend(glob(join(images_dir, ext)))
-
-
+        if input_data_source == 'tfrecords':
+            dataset = parse_tfrecords(tfrecords_dir, image_size=size, max_bboxes=yolo_max_boxes, class_file=None)
+            for index, entry in enumerate(dataset):
+                annotated_image, image_detections_result = self._do_detection(model, entry[0], size, class_names,
+                                                                              yolo_max_boxes, bbox_color, font_size,
+                                                                              index)
+                self._dump_detections_text(image_detections_result, print_detections, detections_list_outfile)
+                out_filename = f'detect_{index}.jpg'
+                self._output_annotated_image(annotated_image, output_dir, out_filename, save_result_images,
+                                             display_result_images)
         else:
-            raise Exception(f'input_data_source {input_data_source} not valid')
+            if input_data_source == 'image_file':
+                filenames = [image_file_path]
+            elif input_data_source == 'images_dir':
+                # dirname = images_dir
 
-        for index, file in enumerate(filenames):
-            img_raw = tf.image.decode_image(open(file, 'rb').read(), channels=3)
-            annotated_image, image_detections_result = inferecnce.do_inference(model, img_raw / 255, size, class_names,
-                                                                               yolo_max_boxes, bbox_color, font_size,
-                                                                               index)
-            inferecnce.output_detections_text(image_detections_result)
-            inferecnce.output_annotated_image(annotated_image, out_filename=f'detect_{index}.jpg')
+                import glob
+                types = ('*.jpeg', '*.jpg', '*.png', '*.bmp')  # the tuple of file types
+                filenames = []
+                for files in types:
+                    filenames.extend(glob.glob(files))
+
+                from os.path import join
+                from glob import glob
+
+                for ext in ('*.jpeg', '*.jpg', '*.png', '*.bmp'
+                            ):
+                    filenames.extend(glob(join(images_dir, ext)))
+            else:
+                raise Exception(f'input_data_source {input_data_source} not valid')
+
+            for index, file in enumerate(filenames):
+                img_raw = tf.image.decode_image(open(file, 'rb').read(), channels=3)
+                annotated_image, image_detections_result = self._do_detection(model, img_raw / 255, size, class_names,
+                                                                              yolo_max_boxes, bbox_color, font_size,
+                                                                              index)
+                self._dump_detections_text(image_detections_result, print_detections, detections_list_outfile)
+                out_filename = f'detect_{index}.jpg'
+                self._output_annotated_image(annotated_image, output_dir, out_filename, save_result_images,
+                                             display_result_images)
 
 
 parser = argparse.ArgumentParser()
@@ -143,4 +141,5 @@ config_file = args.config
 with open(config_file, 'r') as stream:
     detect_config = yaml.safe_load(stream)
 
-detect(**detect_config)
+inference = Inference()
+inference(**detect_config)
--- train.py
+++ train.py
@@ -25,11 +25,10 @@ import yaml
 import argparse
 import matplotlib.pyplot as plt
 
-
 from utils import get_anchors
 from render_utils import render_bboxes
 
-from preprocess_dataset import preprocess_dataset, preprocess_dataset_debug
+from preprocess_dataset import PreprocessDataset
 from loss_func import get_loss_func
 from models import YoloV3Model
 
@@ -37,196 +36,201 @@ from load_tfrecords import parse_tfrecords
 from load_dataset import load_dataset, load_debug_dataset
 
 
+class Train:
+    @staticmethod
+    def _calc_loss(model, images, labels, loss_fn_list, batch_size):
+        outputs = model(images, training=True)
+        regularization_loss = tf.reduce_sum(model.losses)
+        pred_loss = []
+        for output, label, loss_fn in zip(outputs, labels, loss_fn_list):
+            pred_loss.append(loss_fn(label, output) / batch_size)
+        pred_loss_stack_all = tf.stack(pred_loss, axis=1)
+        pred_loss_per_grid = tf.reduce_sum(
+            pred_loss_stack_all, axis=0)
+        pred_loss_per_source = tf.reduce_sum(
+            pred_loss_stack_all, axis=1)
 
-def loss_calc(model, images, labels, loss_fn_list, batch_size):
-    outputs = model(images, training=True)
-    regularization_loss = tf.reduce_sum(model.losses)
-    pred_loss = []
-    for output, label, loss_fn in zip(outputs, labels, loss_fn_list):
-        pred_loss.append(loss_fn(label, output) / batch_size)
-    pred_loss_stack_all = tf.stack(pred_loss, axis=1)
-    pred_loss_per_grid = tf.reduce_sum(
-        pred_loss_stack_all, axis=0)
-    pred_loss_per_source = tf.reduce_sum(
-        pred_loss_stack_all, axis=1)
+        total_loss = tf.reduce_sum(
+            pred_loss_per_source) + regularization_loss
 
-    total_loss = tf.reduce_sum(
-        pred_loss_per_source) + regularization_loss
+        return total_loss, pred_loss, pred_loss_per_grid, pred_loss_per_source
 
-    return total_loss, pred_loss, pred_loss_per_grid, pred_loss_per_source
+    def _train_eager_mode(self, model, ds_train, ds_val, loss_fn_list, optimizer, batch_size, epochs,
+                          checkpoits_path_prefix):
+        global_steps = tf.Variable(1, trainable=False, dtype=tf.int64)
+        for epoch in range(1, epochs + 1):
+            for batch, (images, labels) in enumerate(ds_train):
+                with tf.GradientTape() as tape:
+                    total_loss, pred_loss, pred_loss_per_grid, pred_loss_per_source = self._calc_loss(model, images,
+                                                                                                      labels,
+                                                                                                      loss_fn_list,
+                                                                                                      batch_size)
 
+                grads = tape.gradient(total_loss, model.trainable_variables)
+                optimizer.apply_gradients(
+                    zip(grads, model.trainable_variables))
 
-def train_eager_mode(model, ds_train, ds_val, loss_fn_list, optimizer, batch_size, epochs, checkpoits_path_prefix):
-    global_steps = tf.Variable(1, trainable=False, dtype=tf.int64)
-    for epoch in range(1, epochs + 1):
-        for batch, (images, labels) in enumerate(ds_train):
-            with tf.GradientTape() as tape:
-                total_loss, pred_loss, pred_loss_per_grid, pred_loss_per_source = loss_calc(model, images,
-                                                                                            labels,
-                                                                                            loss_fn_list,
-                                                                                            batch_size)
+                logging.info(
+                    f'{epoch}_train_{batch}_lr:{optimizer.lr.numpy():.6f}, '
+                    f'totLoss:{total_loss.numpy()}, '
+                    f'perGrid{list(pred_loss_per_grid.numpy())}, '
+                    f'perSource[xy,wh,obj,class]:{pred_loss_per_source.numpy()}, '
+                    f'perGridPerSource:{[list(x.numpy()) for idx, x in enumerate(pred_loss)]}')
 
-            grads = tape.gradient(total_loss, model.trainable_variables)
-            optimizer.apply_gradients(
-                zip(grads, model.trainable_variables))
+                global_steps.assign_add(1)
+            model.save_weights(
+                f'{checkpoits_path_prefix}{epoch}.tf')
 
-            logging.info(
-                f'{epoch}_train_{batch}_lr:{optimizer.lr.numpy():.6f}, '
-                f'totLoss:{total_loss.numpy()}, '
-                f'perGrid{list(pred_loss_per_grid.numpy())}, '
-                f'perSource[xy,wh,obj,class]:{pred_loss_per_source.numpy()}, '
-                f'perGridPerSource:{[list(x.numpy()) for idx, x in enumerate(pred_loss)]}')
+            for batch, (images, labels) in enumerate(ds_val):
+                total_loss, pred_loss, pred_loss_per_grid, pred_loss_per_source = self._calc_loss(model, images,
+                                                                                                  labels,
+                                                                                                  loss_fn_list,
+                                                                                                  batch_size)
 
-            global_steps.assign_add(1)
-        model.save_weights(
-            f'{checkpoits_path_prefix}{epoch}.tf')
+                logging.info(
+                    f'{epoch}_val_{batch}_lr:{optimizer.lr.numpy():.6f}, '
+                    f'totLoss:{total_loss.numpy()}, '
+                    f'perGrid{list(pred_loss_per_grid.numpy())}, '
+                    f'perSource[xy,wh,obj,class]:{pred_loss_per_source.numpy()}, '
+                    f'perGridPerSource:{[list(x.numpy()) for idx, x in enumerate(pred_loss)]}')
 
-        for batch, (images, labels) in enumerate(ds_val):
-            total_loss, pred_loss, pred_loss_per_grid, pred_loss_per_source = loss_calc(model, images,
-                                                                                        labels,
-                                                                                        loss_fn_list,
-                                                                                        batch_size)
+    @staticmethod
+    def _count_file_lines(filename):
+        with open(filename, 'r') as fp:
+            nlines = len(fp.readlines())
+        return nlines
 
-            logging.info(
-                f'{epoch}_val_{batch}_lr:{optimizer.lr.numpy():.6f}, '
-                f'totLoss:{total_loss.numpy()}, '
-                f'perGrid{list(pred_loss_per_grid.numpy())}, '
-                f'perSource[xy,wh,obj,class]:{pred_loss_per_source.numpy()}, '
-                f'perGridPerSource:{[list(x.numpy()) for idx, x in enumerate(pred_loss)]}')
+    class WeightsSaver(Callback):
+        def __init__(self, n):
+            super(Callback, self).__init__()
+            self.n = n
+            self.epoch = 0
 
+        def on_epoch_end(self, epoch, logs={}):
+            if self.epoch % self.n == 0:
+                name = 'checkpoints/yolov3_train.tf'
+                self.model.save_weights(name)
+                self.epoch += 1
 
-def count_file_lines(filename):
-    with open(filename, 'r') as fp:
-        nlines = len(fp.readlines())
-    return nlines
+    def __call__(self, input_data_source,
+                 image_size,
+                 batch_size,
+                 max_bboxes,
+                 debug_mode,
+                 anchors_file,
+                 learning_rate,
+                 early_stop_patience,
+                 epochs,
+                 mode,
+                 render_dataset_example,
+                 dataset_cuttof_size,
+                 dataset_repeats,
+                 load_weights,
+                 annotations_path,
+                 images_dir,
+                 tfrecords_base_dir,
+                 classes_name_file,
+                 save_checkpoits_path_prefix,
+                 load_checkpoints_path,
+                 early_stopping
+                 ):
+        grid_sizes_table = np.array([13, 26, 52])
 
-class WeightsSaver(Callback):
-  def __init__(self, n):
-    self.n = n
-    self.epoch = 0
+        logging.basicConfig(level=logging.INFO,
+                            format='%(levelname)s %(message)s',
+                            )
+        physical_devices = tf.config.experimental.list_physical_devices('GPU')
 
-  def on_epoch_end(self, epoch, logs={}):
-    if self.epoch % self.n == 0:
-        name = 'checkpoints/yolov3_train.tf'
-        self.model.save_weights(name)
-        self.epoch += 1
+        dataset = []
+        if input_data_source == 'tfrecords':
+            tfrecords_dir_train = f'{tfrecords_base_dir}/train'
+            tfrecords_dir_val = f'{tfrecords_base_dir}/val'
+            for ds_dir in [tfrecords_dir_train, tfrecords_dir_val]:
+                dset = parse_tfrecords(
+                    ds_dir, image_size, max_bboxes, classes_name_file)
+                dataset.append(dset)
+        elif input_data_source == 'images_dir_annot_file':
+            train_dataset, val_dataset, test_dataset, train_size, val_size = load_dataset(images_dir, annotations_path,
+                                                                                          classes_name_file, image_size,
+                                                                                          dataset_cuttof_size=dataset_cuttof_size,
+                                                                                          max_bboxes=100,
+                                                                                          train_split=0.7,
+                                                                                          val_split=0.2)
 
-def train(input_data_source,
-          image_size,
-          batch_size,
-          max_bboxes,
-          debug_mode,
-          anchors_file,
-          learning_rate,
-          early_stop_patience,
-          epochs,
-          mode,
-          render_dataset_example,
-          dataset_cuttof_size,
-          dataset_repeats,
-          load_weights,
-          annotations_path,
-          images_dir,
-          tfrecords_base_dir,
-          classes_name_file,
-          save_checkpoits_path_prefix,
-          load_checkpoints_path,
-          early_stopping
-          ):
-    grid_sizes_table = np.array([13, 26, 52])
+            # Modify batch size, to avoid an empty dataset after batching with drop_remainder=True:
+            batch_size = batch_size if (batch_size <= min(train_size, val_size)) else min(train_size, val_size)
 
-    logging.basicConfig(level=logging.INFO,
-                        format='%(levelname)s %(message)s',
-                        )
-    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+        else:  # debug_data
+            train_dataset = load_debug_dataset()
+            val_dataset = load_debug_dataset()
+        if not len(dataset):
+            dataset = [train_dataset, val_dataset]
 
-    dataset = []
-    if input_data_source == 'tfrecords':
-        tfrecords_dir_train = f'{tfrecords_base_dir}/train'
-        tfrecords_dir_val = f'{tfrecords_base_dir}/val'
-        for ds_dir in [tfrecords_dir_train, tfrecords_dir_val]:
-            dset = parse_tfrecords(
-                ds_dir, image_size, max_bboxes, classes_name_file)
-            dataset.append(dset)
-    elif input_data_source == 'images_dir_annot_file':
-        train_dataset, val_dataset, test_dataset, train_size, val_size = load_dataset(images_dir, annotations_path,
-                                                                                      classes_name_file, image_size,
-                                                                                      dataset_cuttof_size=dataset_cuttof_size,
-                                                                                      max_bboxes=100, train_split=0.7,
-                                                                                      val_split=0.2)
+        if dataset_repeats:  # repeat train and val
+            dataset[0] = dataset[0].repeat(dataset_repeats)
+            dataset[1] = dataset[1].repeat(dataset_repeats)
 
-        # Modify batch size, to avoid an empty dataset after batching with drop_remainder=True:
-        batch_size = batch_size if (batch_size <= min(train_size, val_size)) else min(train_size, val_size)
+        anchors_table = get_anchors(anchors_file)
+        nclasses = self._count_file_lines(classes_name_file)
 
-    else: #debug_data
-        train_dataset = load_debug_dataset()
-        val_dataset = load_debug_dataset()
-    if not len(dataset):
-        dataset = [train_dataset, val_dataset]
+        if render_dataset_example:
+            x_train, bboxes = next(iter(dataset[0]))
+            image = render_bboxes(x_train[tf.newaxis, ...], bboxes[tf.newaxis, ...], colors=[(255, 255, 255)])
+            plt.imshow(image)
+            plt.show()
 
-    if dataset_repeats:  # repeat train and val
-        dataset[0] = dataset[0].repeat(dataset_repeats)
-        dataset[1] = dataset[1].repeat(dataset_repeats)
+        yolov3_model = YoloV3Model()
+        model = yolov3_model(image_size, nclasses=nclasses)
 
-    anchors_table = get_anchors(anchors_file)
-    nclasses = count_file_lines(classes_name_file)
+        with open("model_summary.txt", "w") as file1:
+            model.summary(print_fn=lambda x: file1.write(x + '\n'))
 
-    if render_dataset_example:
-        x_train, bboxes = next(iter(dataset[0]))
-        image = render_bboxes(x_train[tf.newaxis, ...], bboxes[tf.newaxis, ...])
-        plt.imshow(image)
-        plt.show()
+        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
-    yolov3_model = YoloV3Model()
-    model = yolov3_model(image_size, nclasses=nclasses)
+        loss_fn_list = [get_loss_func(anchors, nclasses, tf.constant(mode == 'eager_tf', dtype=tf.bool))
+                        for anchors in anchors_table]
 
-    with open("model_summary.txt", "w") as file1:
-        model.summary(print_fn=lambda x: file1.write(x + '\n'))
+        model.compile(optimizer=optimizer, loss=loss_fn_list,
+                      run_eagerly=(mode == 'eager_fit'))
+        preprocess_dataset = PreprocessDataset()
+        if debug_mode:
+            preprocess_dataset.preprocess_dataset_debug(dataset[0], batch_size, image_size, anchors_table,
+                                                        grid_sizes_table,
+                                                        max_bboxes)
+        ds_preprocessed = []
+        for ds_split in dataset:
+            data = preprocess_dataset(ds_split, batch_size, image_size, anchors_table, grid_sizes_table,
+                                      max_bboxes)
+            ds_preprocessed.append(data)
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+        ds_train, ds_val = ds_preprocessed
 
-    loss_fn_list = [get_loss_func(anchors, nclasses, tf.constant(mode == 'eager_tf', dtype=tf.bool))
-                    for anchors in anchors_table]
+        if load_weights:
+            model.load_weights(load_checkpoints_path)
 
-    model.compile(optimizer=optimizer, loss=loss_fn_list,
-                  run_eagerly=(mode == 'eager_fit'))
+        if mode == 'eager_tf':
+            self._train_eager_mode(model, ds_train, ds_val, loss_fn_list, optimizer, batch_size, epochs,
+                                   save_checkpoits_path_prefix)
+        else:
 
-    if debug_mode:
-        preprocess_dataset_debug(dataset[0], batch_size, image_size, anchors_table, grid_sizes_table,
-                                 max_bboxes)
-    ds_preprocessed = []
-    for ds_split in dataset:
-        data = preprocess_dataset(ds_split, batch_size, image_size, anchors_table, grid_sizes_table,
-                                  max_bboxes)
-        ds_preprocessed.append(data)
+            callbacks = [
+                # ReduceLROnPlateau(verbose=1),
+                # ModelCheckpoint('checkpoints/yolov3_train.tf',
+                #                 verbose=1, save_weights_only=True),
+                # TensorBoard(log_dir='logs')
+                self.WeightsSaver(2)
+            ]
+            from keras.callbacks import Callback
 
-    ds_train, ds_val = ds_preprocessed
+            if early_stopping:
+                callbacks.append(
+                    EarlyStopping(patience=early_stop_patience, restore_best_weights=True, monitor='val_loss',
+                                  verbose=1))
 
-    if load_weights:
-        model.load_weights(load_checkpoints_path)
-
-    if mode == 'eager_tf':
-        train_eager_mode(model, ds_train, ds_val, loss_fn_list, optimizer, batch_size, epochs,
-                         save_checkpoits_path_prefix)
-    else:
-
-        callbacks = [
-            # ReduceLROnPlateau(verbose=1),
-            # ModelCheckpoint('checkpoints/yolov3_train.tf',
-            #                 verbose=1, save_weights_only=True),
-            # TensorBoard(log_dir='logs')
-            WeightsSaver(2)
-        ]
-        from keras.callbacks import Callback
-
-        if early_stopping:
-            callbacks.append(
-                EarlyStopping(patience=early_stop_patience, restore_best_weights=True, monitor='val_loss', verbose=1))
-
-        history = model.fit(ds_train,
-                            epochs=epochs,
-                            callbacks=callbacks,
-                            validation_data=ds_val)
+            history = model.fit(ds_train,
+                                epochs=epochs,
+                                callbacks=callbacks,
+                                validation_data=ds_val)
 
 
 if __name__ == '__main__':
@@ -239,4 +243,6 @@ if __name__ == '__main__':
 
     with open(config_file, 'r') as stream:
         train_config = yaml.safe_load(stream)
+
+    train = Train()
     train(**train_config)
