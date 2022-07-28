c6e2d1a5707bb6afe9ff98f958face991d6df596
ronen-halevy <ronen567@gmail.com>
2022-07-29 00:14:22 +0300

    convert cosmetics

--- utilities/convert.py
+++ utilities/convert.py
@@ -1,34 +1,19 @@
-from absl import app, logging
-from absl.flags import FLAGS
-import numpy as np
-
 import tensorflow as tf
+import numpy as np
 import yaml
 import argparse
 
 from core.models import YoloV3Model
-from core.utils import get_anchors
 
-
-YOLOV3_LAYER_LIST = [
-    'yolo_darknet',
-    'yolo_conv_0',
-    'yolo_output_0',
-    'yolo_conv_1',
-    'yolo_output_1',
-    'yolo_conv_2',
-    'yolo_output_2',
-]
 
 def load_darknet_weights(model, weights_file):
     wf = open(weights_file, 'rb')
     major, minor, revision, seen, _ = np.fromfile(wf, dtype=np.int32, count=5)
-
-    layers = YOLOV3_LAYER_LIST
-    layers = ['darknet53', 'intermediate_out0', 'grid_pred0',  'intermediate_out1','grid_pred1', 'intermediate_out2',
-     'grid_pred2']
+    layers = [model.layers[idx].name for idx in range(len(model.layers))]
     for layer_name in layers:
         sub_model = model.get_layer(layer_name)
+        if not hasattr(sub_model, 'layers'):
+            continue
         for i, layer in enumerate(sub_model.layers):
             if not layer.name.startswith('conv2d'):
                 continue
@@ -37,7 +22,7 @@ def load_darknet_weights(model, weights_file):
                     sub_model.layers[i + 1].name.startswith('batch_norm'):
                 batch_norm = sub_model.layers[i + 1]
 
-            logging.info("{}/{} {}".format(
+            print("{}/{} {}".format(
                 sub_model.name, layer.name, 'bn' if batch_norm else 'bias'))
 
             filters = layer.filters
@@ -71,13 +56,10 @@ def load_darknet_weights(model, weights_file):
     wf.close()
 
 
-
-
-def convert(image_size, nclasses, input_weights_file, output_weights_file):
+def convert(nclasses, input_weights_file, output_weights_file):
     physical_devices = tf.config.experimental.list_physical_devices('GPU')
     if len(physical_devices) > 0:
         tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
 
     yolov3_model = YoloV3Model()
     model = yolov3_model(nclasses=nclasses)
@@ -89,7 +71,6 @@ def convert(image_size, nclasses, input_weights_file, output_weights_file):
     img = np.random.random((1, 320, 320, 3)).astype(np.float32)
     output = model(img)
     logging.info('sanity check passed')
-
     model.save_weights(output_weights_file)
     logging.info('weights saved')
 
@@ -97,8 +78,7 @@ def convert(image_size, nclasses, input_weights_file, output_weights_file):
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--config", type=str, default='convert_config.yaml',
-                        help='yaml config file')
-
+                    help='yaml config file')
 
 args = parser.parse_args()
 config_file = args.config
@@ -106,12 +86,7 @@ config_file = args.config
 with open(config_file, 'r') as stream:
     convert_config = yaml.safe_load(stream)
 
-image_size=416
-nclasses=80
-weights_file = '/home/ronen/PycharmProjects/yolo-v3-tf2/weights/coco/coco2012/yolov3.weights'
-output_weights_file = '/home/ronen/PycharmProjects/yolo-v3-tf2/weights/coco/coco2012/myoutput_yolov3.tf'
-convert(image_size, nclasses, weights_file, output_weights_file)
-
-
-
-
+nclasses = convert_config['num_classes']
+weights_file = convert_config['weights_file']
+output_weights_file = convert_config['output_weights_file']
+convert(nclasses, weights_file, output_weights_file)
