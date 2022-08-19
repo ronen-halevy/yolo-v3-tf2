c4f590712fd1746845be732b92007502df92ccdd
ronen-halevy <ronen567@gmail.com>
2022-08-19 16:49:49 +0300

    rename convert

--- utilities/convert.py
+++ utilities/convert.py
@@ -1,185 +1,113 @@
 import tensorflow as tf
+from tensorflow.keras import Input
 import numpy as np
 import yaml
 import argparse
 
-from core.models import YoloV3Model
-
-layers_lists = [
-    'darknet53',
-    'neck0',
-    'head0',
-    'neck1',
-    'head1',
-    'neck2',
-    'head2',
-]
-
 
 class Convert:
+
     @staticmethod
-    def __process_layer(wf, layer, layers, layer_idx):
-        """
-        Load weights from a file to conv2d layers. A conv2d weigt entries may either be list of [bias, weight] or,
-        in case where a bn module follows, a single [weight] value
-        In the latter case, instead of loading bias, the batch_normalization entry is loaded.
-        :param wf:  weights input file, darknet format (=caffe)
-        :type wf: Buffered Reader
-        :param layer: Keras layer
-        :type layer: Keras layer
-        :param layers: The list of model's layers (maybe a sub-model as well). Needed to extract bn weights, if
-         bn follows.
-        :type layers: List of Keras layer
-        :param layer_idx: Index of current layer within layers list. Needed to locate next layer - for he bn case
-        :type layer_idx:
-        :return:
-        :rtype:
-        """
-        if layer.name.startswith('conv2d'):
-            filters = layer.filters
-            size = layer.kernel_size[0]
-            in_dim = layer.get_input_shape_at(0)[-1]
+    def find_next_layer(model, layer_name):
+        for layer_index, layer in enumerate(model.layers):
+            if 'input' in layer.name:  # input is the first layer, so never the next
+                continue
+            try:
+                if hasattr(layer, 'input'):
+                    if isinstance(layer.input, list):
+                        for input_layer in layer.input:
+                            if input_layer.name.startswith(f'{layer_name}'):
+                                return layer
+                    else:
+                        if layer.input.name.startswith(f'{layer_name}'):
+                            return layer
 
-            if len(layer.weights) == 1:  # ebtry contains a weight w/o a bias, which is the case of bn in next layer.
-                # darknet [beta, gamma, mean, variance]
-                bn_weights = np.fromfile(
-                    wf, dtype=np.float32, count=4 * filters)
-                # tf [gamma, beta, mean, variance]
-                bn_weights = bn_weights.reshape((4, filters))[[1, 0, 2, 3]] # bn weights
-                batch_norm = layers[layer_idx + 1] # targer bn entry
-            else:  # a bias + weight layer
-                conv_bias = np.fromfile(wf, dtype=np.float32, count=filters)
+            except Exception as e:
+                print(e)
 
-            print(f'{layer.name} {"bn" if len(layer.weights) == 1 else "bias"}')
+        return None
 
-            # darknet shape (out_dim, in_dim, height, width)
-            conv_shape = (filters, in_dim, size, size)
-            conv_weights = np.fromfile(
-                wf, dtype=np.float32, count=np.product(conv_shape))
-            # DarkNet conv_weights are serialized Caffe-style:
-            # (out_dim, in_dim, height, width)
-            # We would like to set these to Tensorflow order:
-            # (height, width, in_dim, out_dim)
-            conv_weights = conv_weights.reshape(
-                conv_shape).transpose([2, 3, 1, 0])
-            if len(layer.weights) == 2:
-                layer.set_weights([conv_weights, conv_bias])
-            else:
-                layer.set_weights([conv_weights])
-                batch_norm.set_weights(bn_weights)
+    def load_conv_layer(self, model, layer, wf):
+        print(layer.name)
+        bn = False
+        next_layer = self.find_next_layer(model, layer.name)
 
-    def __recurse_model(self, wf, model):
-        """
-        A recursive method - Loop on model's layers, call __process_layer which loads weights to layer from file
-        :param wf: weights input file, darknet format (=caffe)
-        :type wf: Buffered Reader
-        :param model:  A model can be either a model which contains layers and sub-models or a descrete layer.
-        If a model
-         call __recurse_model
-        :type model: Keras model or Leras layer
-        :return:
-        :rtype:
-        """
+        filters = layer.filters
+        size = layer.kernel_size[0]
+        in_dim = layer.get_input_shape_at(0)[-1]
 
-        for layer_idx, layer in enumerate(model.layers):
-            if not hasattr(layer, 'layers'):
-                self.__process_layer(wf, layer, model.layers, layer_idx)
-            else:
-                print(f'++ start layer of model {layer.name} ++')
-                self.__recurse_model(wf, layer)
-                print(f'-- done model {layer.name} --')
+        if next_layer and next_layer.name.startswith('batch_norm'):
+            print(next_layer.name)
 
-        # for layer_idx, layer in enumerate(model.layers):
-        #     if isinstance(layer, tf.keras.Model):
-        #         print(f'++ start layer of model {layer.name} ++')
-        #         self.__recurse_model(wf, layer)
-        #         print(f'-- done model {layer.name} --')
-        #     # if not hasattr(layer, 'layers'):
-        #     else:
-        #         self.__process_layer(wf, layer, model.layers, layer_idx)
+            bn = True
+            bn_weights = np.fromfile(
+                wf, dtype=np.float32, count=4 * filters)
+            # tf [gamma, beta, mean, variance]
+            bn_weights = bn_weights.reshape((4, filters))[[1, 0, 2, 3]]
+        else:
+            conv_bias = np.fromfile(wf, dtype=np.float32, count=filters)
 
-    def load_darknet_weights1(self, model, weights_file, tiny=False):
+        conv_shape = (filters, in_dim, size, size)
+        conv_weights = np.fromfile(
+            wf, dtype=np.float32, count=np.product(conv_shape))
+        conv_weights = conv_weights.reshape(
+            conv_shape).transpose([2, 3, 1, 0])
+
+        if bn:
+            layer.set_weights([conv_weights])
+            next_layer.set_weights(bn_weights)
+        else:
+            layer.set_weights([conv_weights, conv_bias])
+
+    def load_sub_model_layers(self, model, wf):
+        count = 0
+        for idx in range(len(model.layers)):
+            if 'conv2d' in model.layers[idx].name:
+                self.load_conv_layer(model, model.layers[idx], wf)
+                count = count + 1
+        return count
+
+    def load_sub_model(self, sub_model, next_conv_layer_name, wf):
+        conv_layers_count = 0
+        for jdx in range(len(sub_model.layers)):
+            if next_conv_layer_name == sub_model.layers[jdx].name:
+                conv_layers_count = self.load_sub_model_layers(sub_model, wf)
+                print(f'Sub-model name: {sub_model.name}, Conv Layers Loaded: {conv_layers_count}')
+                break
+        return conv_layers_count
+
+    def load_darknet_weights(self, model, weights_file):
         wf = open(weights_file, 'rb')
         major, minor, revision, seen, _ = np.fromfile(wf, dtype=np.int32, count=5)
+        search_conv_layers = True
+        next_conv_layer_name = 'conv2d'
 
-        layers = layers_lists
+        next_index = 0
+        while (search_conv_layers == True):
+            conv_layers_count = 0
+            for idx in range(len(model.layers)):
+                if hasattr(model.layers[idx], 'layers'):
+                    conv_layers_count = self.load_sub_model(model.layers[idx], next_conv_layer_name, wf)
+                    if conv_layers_count:
+                        next_index += conv_layers_count
+                        next_conv_layer_name = f'conv2d_{next_index}'
+                        break
+                elif model.layers[idx].name == next_conv_layer_name:
+                    self.load_conv_layer(model, model.layers[idx], wf)
+                    conv_layers_count = 1
+                    next_index += conv_layers_count
+                    next_conv_layer_name = f'conv2d_{next_index}'
+                    print(f'Descrete Layer name: {model.layers[idx].name}, Loaded')
+                    break
+            if conv_layers_count == 0:
+                search_conv_layers = False
 
-        for layer_name in layers:
-            sub_model = model.get_layer(layer_name)
-            for i, layer in enumerate(sub_model.layers):
-                if not layer.name.startswith('conv2d'):
-                    continue
-                batch_norm = None
-                if i + 1 < len(sub_model.layers) and \
-                        sub_model.layers[i + 1].name.startswith('batch_norm'):
-                    batch_norm = sub_model.layers[i + 1]
-
-                print("{}/{} {}".format(
-                    sub_model.name, layer.name, 'bn' if batch_norm else 'bias'))
-
-                filters = layer.filters
-                size = layer.kernel_size[0]
-                in_dim = layer.get_input_shape_at(0)[-1]
-
-                if batch_norm is None:
-                    conv_bias = np.fromfile(wf, dtype=np.float32, count=filters)
-                else:
-                    # darknet [beta, gamma, mean, variance]
-                    # Darknet serializes convolutional weights as:
-                    # [beta, gamma, mean, variance, conv_weights]
-
-                    bn_weights = np.fromfile(
-                        wf, dtype=np.float32, count=4 * filters)
-                    # tf [gamma, beta, mean, variance]
-                    bn_weights = bn_weights.reshape((4, filters))[[1, 0, 2, 3]]
-
-                # darknet shape (out_dim, in_dim, height, width)
-                conv_shape = (filters, in_dim, size, size)
-                conv_weights = np.fromfile(
-                    wf, dtype=np.float32, count=np.product(conv_shape))
-                # tf shape (height, width, in_dim, out_dim)
-                conv_weights = conv_weights.reshape(
-                    conv_shape).transpose([2, 3, 1, 0])
-
-                if batch_norm is None:
-                    layer.set_weights([conv_weights, conv_bias])
-                else:
-                    layer.set_weights([conv_weights])
-                    batch_norm.set_weights(bn_weights)
-
-        assert len(wf.read()) == 0, 'failed to read all data'
-        wf.close()
-
-    def __load_darknet_weights(self, model, weights_file):
-        wf = open(weights_file, 'rb')
-        major, minor, revision, seen, _ = np.fromfile(wf, dtype=np.int32, count=5)
-        self.__recurse_model(wf, model)
-        assert len(wf.read()) == 0, 'failed to read all data'
-        wf.close()
-
-    def __call__(self, nclasses, input_weights_file, output_weights_file):
-        physical_devices = tf.config.experimental.list_physical_devices('GPU')
-        if len(physical_devices) > 0:
-            tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
-        yolov3_model = YoloV3Model()
-        model = yolov3_model(nclasses=nclasses)
-        model.summary()
-
-        # self.__load_darknet_weights(model, input_weights_file)
-        self.load_darknet_weights1(model, input_weights_file)
-        print('weights loaded')
-
-        img = np.random.random((1, 320, 320, 3)).astype(np.float32)
-        output = model(img)
-        print('sanity check passed')
-        model.save_weights(output_weights_file)
-        print('weights saved')
+        return model
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, default='convert_config.yaml',
+    parser.add_argument("--config", type=str, default='utilities/convert_config.yaml',
                         help='yaml config file')
 
     args = parser.parse_args()
@@ -190,8 +118,24 @@ def main():
     nclasses = convert_config['num_classes']
     weights_file = convert_config['weights_file']
     output_weights_file = convert_config['output_weights_file']
+    model_config_file = convert_config['model_config_file']
+
+    from core.parse_model import ParseModel
+    parse_model = ParseModel()
+
+    with open(model_config_file, 'r') as _stream:
+        model_config = yaml.safe_load(_stream)
+    inputs = Input(shape=(None, None, 3))
+    model = parse_model.build_model(inputs, nclasses, **model_config)
+
     convert = Convert()
-    convert(nclasses, weights_file, output_weights_file)
+    model = convert.load_darknet_weights(model, weights_file)
+    img = np.random.random((1, 320, 320, 3)).astype(np.float32)
+    _ = model(img)
+    print('sanity check passed')
+    model.save_weights(output_weights_file)
+    print('weights saved')
+
 
 if __name__ == "__main__":
     main()
