ebaa441bb9c95ce27677f689ee6f10575b216988
ronen-halevy <ronen567@gmail.com>
2022-08-31 17:03:55 +0300

    wip evaluate

--- evaluate2.py
+++ evaluate2.py
@@ -54,39 +54,182 @@ class Evaluate:
 
         return int_area / (box_1_area + box_2_area - int_area)
 
+    def inc_left_arg(self, left_arg, right_arg):
+        counter = tf.math.add(left_arg,1)
+        return counter, right_arg
 
-    def calc_tp_fp_fn(self, pred_y, true_y, nclasses):
+    def inc_right_arg(self, left_arg, right_arg):
+        counter = tf.math.add(right_arg,1)
+        return counter, right_arg
+
+    def inc_arg(self, arg, val=1):
+        counter = tf.math.add(arg,val)
+        return counter
+    def inc_args(self, args, val=1):
+        counter = tf.map_fn(n=lambda t: tf.math.add(t,val), elems=args, parallel_iterations=3)
+        return counter
+    def nop_func(self, arg):
+        return arg
+
+    def tp_fp_counts(self, result, class_index, tp, fp):
+        t = result == class_index
+        # self.inc_count(tp[class_index])
+        # tf.cond(result, true_fn=lambda: self.inc_count(tp[class_index]), false_fn=lambda: self.inc_count(fp[class_index]))
+
+    def do_iou(self, pred_y, true_y, tp, fp, fn, objects_count):
+        t_unassigned = tf.Variable(tf.fill(tf.shape(true_y[...,1]), 1))
+
         t_bboxes, t_confidences, t_classes_indices = tf.split(true_y, [4,1,1], axis=-1)
-        p_bboxes, p_classes_indices = tf.split(tf.squeeze(pred_y, axis=0), [4,1], axis=-1)
+        # p_bboxes, p_classes_indices = tf.split(tf.squeeze(pred_y, axis=0), [4,1], axis=-1)
+        p_bboxes, p_classes_indices = tf.split(pred_y, [4,1], axis=-1)
 
-        tp = [0] * nclasses
-        fp = [0] * nclasses
-        fn = [0] * nclasses
+        iou = tf.map_fn(fn=lambda t: self.broadcast_iou(t, t_bboxes), elems=p_bboxes, parallel_iterations=3)
+        best_iou_index = tf.math.argmax(iou, axis=-1, output_type=tf.int32).numpy()
+        best_iou_index = tf.cast(tf.squeeze(best_iou_index, axis=-1), tf.int32)
+        t_classes_indices =  tf.cast(tf.squeeze(t_classes_indices, axis=-1), tf.int32)
+        true_class = [t_classes_indices[idx] for idx in best_iou_index]
+        iou = tf.squeeze(iou, axis=1)
+        sel_iou = np.ndarray([tf.shape(iou)[0]])
+        ## ronen todo gather?
+        for idx, (iou_, sel_index) in enumerate(zip(iou, best_iou_index)):
+            sel_iou[idx] = iou_[sel_index]
 
-        t_assigned_to_pred = [False] * tf.shape(t_confidences).numpy()[0]
+        valid_detection = sel_iou > self.iou_thresh
+        true_class = tf.convert_to_tensor(true_class)
+        p_classes_indices = tf.cast(tf.squeeze(p_classes_indices, axis=-1), tf.int32)
+        valid_class = true_class == p_classes_indices
+        res = tf.math.logical_and(valid_detection, valid_class)
+        t_unassigned_sel = tf.gather(t_unassigned, best_iou_index)
+        res = tf.math.logical_and(res, tf.cast(t_unassigned_sel, bool))
+        tf.map_fn(fn=lambda t: t_unassigned[t].assign(0), elems=best_iou_index, parallel_iterations=3)
 
-        for p_bbox, p_class_index  in zip(p_bboxes, p_classes_indices):
-            if tf.shape(t_bboxes)[0] != 0:
-                iou = self.broadcast_iou(p_bbox, t_bboxes)
-            else:
-                fp += 1
-                continue
-            best_iou_index = tf.math.argmax(
-                    iou, axis=-1, output_type=tf.int32).numpy()[0]
-            true_class =  int(t_classes_indices[best_iou_index].numpy()[0])
 
-            if iou[...,best_iou_index] > self.iou_thresh and true_class == p_class_index and not t_assigned_to_pred[best_iou_index]:
-                t_assigned_to_pred[best_iou_index]=True
+        updates = tf.cast(res, dtype=tf.int32)
+        # tf.ones(tf.shape(p_classes_indices)[0], dtype=tf.int32)
+        indices = tf.expand_dims(p_classes_indices, axis=-1)
+        tp = tf.tensor_scatter_nd_add(tp, indices, updates)
+        updates = tf.cast(tf.math.logical_not(res), dtype=tf.int32)
+        fp = tf.tensor_scatter_nd_add(fp, indices, updates)
 
-                tp[true_class]+=1
-            else:
-                pass
-                fp[true_class]+=1
-        for status in t_assigned_to_pred:
-            if not status:
-                fn[true_class]+=1
+        # t_unassigned, t_classes_indices)
+        updates = tf.cast(t_unassigned, dtype=tf.int32)
+        indices = tf.expand_dims(t_classes_indices, axis=-1)
+        fn = tf.tensor_scatter_nd_add(fn, indices, updates)
 
-        return tp, fp, fn
+
+        updates = tf.ones(tf.shape(t_classes_indices)[0], dtype=tf.int32)
+        indices = tf.expand_dims(t_classes_indices, axis=-1)
+        objects_count = tf.tensor_scatter_nd_add(objects_count, indices, updates)
+
+
+
+        #
+        # tp, fp = tf.map_fn(fn=lambda t: tf.cond(t[0],
+        #                                true_fn=lambda: tf.tensor_scatter_nd_add(tp, [t[1]], updates)
+        #                                self.inc_left_arg(tp[t[1]], fp[t[1]]),
+        #                                false_fn=lambda: self.inc_right_arg(fp[t[1]], tp[t[1]])),
+        #           elems=(res, p_classes_indices),
+        #           dtype=(tf.int32, tf.int32))
+        #
+        # fn = tf.map_fn(fn=lambda t: tf.cond(t[0],
+        #                                         true_fn=lambda: self.inc_arg(fn[t[1]]),
+        #                                         false_fn=lambda: self.nop_func(fn[t[1]])),
+        #                    elems=(t_unassigned, t_classes_indices),
+        #                    dtype=(tf.int32))
+        #
+        # if tf.shape(tp)[0] != 3:
+        #     print(tf.shape(tp))
+        return tp, fp, fn, objects_count
+        # tp, fp = tf.map_fn(fn=lambda t: tf.cond(t, true_fn=self.inc_count(res) , false_fn=self.inc_count1(res)), elems=res, parallel_iterations=3)
+
+    # def inc_fp(self, fp, classes_indices):
+    #     pass
+    # def calc_tp_fp_fn(self, pred_y, true_y, nclasses):
+    #     # t_bboxes, t_confidences, t_classes_indices = tf.split(true_y, [4,1,1], axis=-1)
+    #     # p_bboxes, p_classes_indices = tf.split(tf.squeeze(pred_y, axis=0), [4,1], axis=-1)
+    #
+    #     tp = tf.convert_to_tensor([0] * nclasses)
+    #     fp = tf.convert_to_tensor([0] * nclasses)
+    #     fn = tf.convert_to_tensor([0] * nclasses)
+    #
+    #     # t_unassigned = tf.Variable(tf.fill(tf.shape(true_y[...,1]), 1))
+    #     # true_y = tf.concat([true_y, t_assigned], -1)
+    #     # t_assigned_to_pred = [False] * tf.shape(t_confidences).numpy()[0]
+    #     # usw tf.cond!!!!
+    #
+    #     tp, fp, fn = tf.cond(tf.shape(true_y)[0] != 0,
+    #                          true_fn=  lambda: self.do_iou(pred_y, true_y, tp, fp, fn),
+    #                          false_fn= lambda: self.inc_arg(fp, pred_y[...,4]))
+    #
+    #     print(tp, fp, fn)
+        # tp = tf.cond(tf.shape(true_y)[0] != 0,
+        #                      true_fn=  lambda: self.do_iou(pred_y, true_y, t_unassigned, tp, fp, fn),
+        #                      false_fn= lambda: self.inc_args(fp, pred_y[...,4]))
+        # a=tf.convert_to_tensor(4)
+        # b = tf.convert_to_tensor(5)
+        # result = tf.cond(tf.convert_to_tensor(True), lambda: tf.math.multiply(a, b), lambda: tf.math.add(a, b))
+        # result = tf.cond(tf.convert_to_tensor(True), lambda: self.do_iou(pred_y, true_y, t_unassigned, tp, fp, fn), lambda: tf.math.add(a, b))
+
+        # tf.cond(True, true_fn=self.do_iou(pred_y, true_y, t_unassigned, tp, fp, fn), false_fn=self.do_iou(pred_y, true_y, t_unassigned, tp, fp, fn))
+
+        # print(x)
+        # pass
+        # if tf.shape(true_y)[0] != 0:
+        #     iou = tf.map_fn(fn=lambda t: self.broadcast_iou(t, t_bboxes), elems=p_bboxes, parallel_iterations=3)
+        #     best_iou_index = tf.math.argmax(iou, axis=-1, output_type=tf.int32).numpy()
+        #     best_iou_index = tf.cast(tf.squeeze(best_iou_index, axis=-1), tf.int32)
+        #     t_classes_indices = tf.squeeze(t_classes_indices, axis=-1)
+        #     true_class = [t_classes_indices[idx] for idx in best_iou_index]
+        #     iou = tf.squeeze(iou, axis=1)
+        #     sel_iou = np.ndarray([tf.shape(iou)[0]])
+        #     for idx, (iou_, sel_index) in enumerate(zip(iou, best_iou_index)):
+        #         sel_iou[idx] = iou_[sel_index]
+        #
+        #
+        #     valid_detection = sel_iou > self.iou_thresh
+        #     true_class = tf.convert_to_tensor(true_class)
+        #     p_classes_indices = tf.squeeze(p_classes_indices, axis=-1)
+        #
+        #     valid_class = true_class==p_classes_indices
+        #     t_not_assigned_to_pred = tf.math.logical_not(tf.convert_to_tensor(t_assigned_to_pred))
+        #     res = tf.math.logical_and(valid_detection,valid_class)
+        #     res = tf.math.logical_and(res,t_not_assigned_to_pred)
+        #     # tp, fp = tf.map_fn(fn=lambda t: tf.cond(t, true_fn=tp[], elems=res, parallel_iterations=3)
+        #
+        #
+        #     print(res)
+
+
+        #     for p_bbox, p_class_index in zip(p_bboxes, p_classes_indices):
+        #         if iou[...,best_iou_index] > self.iou_thresh and true_class == p_class_index and not t_assigned_to_pred[best_iou_index]:
+        #         t_assigned_to_pred[best_iou_index]=True
+        #
+        #
+        # else:
+        #     fp += tf.shape(p_bboxes).numpy()[0]
+        #
+        # for p_bbox, p_class_index  in zip(p_bboxes, p_classes_indices):
+        #     if tf.shape(t_bboxes)[0] != 0:
+        #         iou = self.broadcast_iou(p_bbox, t_bboxes)
+        #     else:
+        #         fp += 1
+        #         continue
+        #     best_iou_index = tf.math.argmax(
+        #             iou, axis=-1, output_type=tf.int32).numpy()[0]
+        #     true_class =  int(t_classes_indices[best_iou_index].numpy()[0])
+        #
+        #     if iou[...,best_iou_index] > self.iou_thresh and true_class == p_class_index and not t_assigned_to_pred[best_iou_index]:
+        #         t_assigned_to_pred[best_iou_index]=True
+        #
+        #         tp[true_class]+=1
+        #     else:
+        #         pass
+        #         fp[true_class]+=1
+        # for status in t_assigned_to_pred:
+        #     if not status:
+        #         fn[true_class]+=1
+        #
+        # return tp, fp, fn
 
 
     @staticmethod
@@ -125,30 +268,39 @@ class Evaluate:
         dataset = parse_tfrecords(tfrecords_dir, image_size=image_size, max_bboxes=yolo_max_boxes, class_file=classes_name_file)
         # mask dataset's padded boxes:
         dataset = dataset.map(lambda x, y: (x, y[y[..., 4] == 1]))
-        model = self.create_model(self, model_config_file, nclasses, nms_score_threshold, nms_iou_threshold)
+        model = self.create_model(model_config_file, nclasses, nms_score_threshold, nms_iou_threshold)
 
         dataset = dataset.map(lambda x, y: (self.inference_func(x, model), y))
 
 
         data = dataset.map(lambda x, y: (tf.concat([x[0], tf.expand_dims(tf.cast(x[2], dtype=tf.float32), axis=-1)], axis=-1), y))
-        tp = fp = fn = [0] * nclasses
-        for idx, (pred_y, true_y) in enumerate(data):
-            tp_update, fp_update, fn_update = self.calc_tp_fp_fn(pred_y, true_y, nclasses)
-            tp = [tp_ + tp_update_ for tp_, tp_update_ in zip(tp, tp_update)]
-            fp = [fp_ + fp_update_ for fp_, fp_update_ in zip(fp, fp_update)]
-            fn = [fn_ + fn_update_ for fn_, fn_update_ in zip(fn, fn_update)]
-        tp = np.array(tp)
-        fp = np.array(fp)
-        fn = np.array(fn)
+        # tp = fp = fn = [0] * nclasses
 
-        hist =  data.map(lambda x, y:  tf.histogram_fixed_width(y[:,5], value_range=[0, nclasses], nbins=nclasses))
-        objects_count = hist.reduce(np.int32(0), lambda x, y: tf.cast(x, tf.int32)+y).numpy()
+
+
+        tp = tf.convert_to_tensor([0] * nclasses)
+        fp = tf.convert_to_tensor([0] * nclasses)
+        fn = tf.convert_to_tensor([0] * nclasses)
+        objects_count = tf.convert_to_tensor([0] * nclasses)
+
+        for idx, (pred_y, true_y) in enumerate(data):
+            pred_y =tf.squeeze(pred_y, axis=0)
+
+            tp, fp, fn, objects_count = tf.cond(tf.shape(true_y)[0] != 0,
+                                 true_fn=lambda: self.do_iou(pred_y, true_y, tp, fp, fn, objects_count),
+                                 false_fn=lambda: self.inc_arg(fp, pred_y[..., 4]))
+
+            # print(tp, fp, fn)
+
+
+        # hist =  data.map(lambda x, y:  tf.histogram_fixed_width(y[:,5], value_range=[0, nclasses], nbins=nclasses))
+        # objects_count = hist.reduce(np.int32(0), lambda x, y: tf.cast(x, tf.int32)+y).numpy()
         tn = objects_count - tp -fp - fn
 
-        accuracy = (tp+tn)/(objects_count + 1e-10)
-        recal = tp / (tp + fn + 1e-20)
-        precision = tp / (tp + fp+1e-20)
-        print(accuracy, recal, precision)
+        accuracy = tf.cast(tp+tn, tf.float32)/(tf.cast(objects_count, tf.float32) + 1e-10)
+        recall = tf.cast(tp, tf.float32) / (tf.cast(tp + fn, tf.float32)  + 1e-20)
+        precision = tf.cast(tp, tf.float32) / (tf.cast(tp + fp, tf.float32)  + 1e-20)
+        print(accuracy, recall, precision)
 
 
 
