9f039f07b21c8e7e0b2d83cbd12ce549493af8d4
ronen-halevy <ronen567@gmail.com>
2022-09-01 15:57:52 +0300

    cosmetics + print images count

--- evaluate2.py
+++ evaluate2.py
@@ -73,31 +73,29 @@ class Evaluate:
 
     # @tf.function
     #todo split the iou func to iou and stats
-    def do_iou(self, pred_y, true_y, tp, fp, fn, all, t_unassigned, errors):
+    def do_iou(self, pred_y, true_y, tp, fp, fn, true_obj, t_unassigned, errors):
         t_bboxes, t_confidences, t_classes_indices = tf.split(true_y, [4,1,1], axis=-1)
         p_bboxes, p_classes_indices = tf.split(pred_y, [4,1], axis=-1)
 
         iou = tf.map_fn(fn=lambda t: self.broadcast_iou(t, t_bboxes), elems=p_bboxes, parallel_iterations=3)
         if tf.equal(tf.size(iou), 0):
-        # if not iou: # todo arrange this - like in start
             fn = tf.tensor_scatter_nd_add(fn, # todo arrange dup counts - + like in start
                                      tf.expand_dims(
                                          tf.cast(true_y[..., 4], tf.int32),
                                          axis=-1),
                                      tf.fill(tf.shape(true_y)[0], 1))
-            all = tf.tensor_scatter_nd_add(all,
+            true_obj = tf.tensor_scatter_nd_add(true_obj,
                                       tf.expand_dims(
                                           tf.cast(true_y[..., 4], tf.int32),
                                           axis=-1),
                                       tf.fill(tf.shape(true_y)[0], 1))
-            return tp, fp, fn, all, errors
+            return tp, fp, fn, true_obj, errors
         try:
             best_iou_index = tf.math.argmax(iou, axis=-1, output_type=tf.int32)#.numpy()
         except Exception as e:
             print(e)
         best_iou_index = tf.cast(tf.squeeze(best_iou_index, axis=-1), tf.int32)
         t_classes_indices =  tf.cast(tf.squeeze(t_classes_indices, axis=-1), tf.int32)
-        # true_class1 = [t_classes_indices[idx] for idx in best_iou_index]
         true_class = tf.gather(t_classes_indices, best_iou_index)
 
         iou = tf.squeeze(iou, axis=1)
@@ -106,38 +104,36 @@ class Evaluate:
         best_iou_index_2d = tf.stack([tf.range(tf.shape(iou)[0]), best_iou_index], axis=-1)
         sel_iou = tf.gather_nd(iou, best_iou_index_2d)
 
-        valid_detection = sel_iou > self.iou_thresh
+        valid_detections = sel_iou > self.iou_thresh
         # true_class = tf.convert_to_tensor(true_class)
         # p_classes_indices = tf.cast(tf.squeeze(p_classes_indices, axis=-1), tf.int32)
         p_classes_indices = tf.cast(p_classes_indices, tf.int32)
         valid_class = true_class == tf.squeeze(p_classes_indices, axis=-1)
-        res = tf.math.logical_and(valid_detection, valid_class)
+        decisions = tf.math.logical_and(valid_detections, valid_class)
 
-        t_unassigned_sel = tf.gather(t_unassigned, best_iou_index)
-        res = tf.math.logical_and(res, tf.cast(t_unassigned_sel, bool))
+        is_not_assigned = tf.gather(t_unassigned, best_iou_index)
+        decisions = tf.math.logical_and(decisions, tf.cast(is_not_assigned, bool))
         tf.map_fn(fn=lambda t: t_unassigned[t].assign(0), elems=best_iou_index, parallel_iterations=3)
 
 
-        updates = tf.cast(res, dtype=tf.int32)
-        # tf.ones(tf.shape(p_classes_indices)[0], dtype=tf.int32)
-        tp = tf.tensor_scatter_nd_add(tp, p_classes_indices, updates)
-        updates = tf.cast(tf.math.logical_not(res), dtype=tf.int32)
-        fp = tf.tensor_scatter_nd_add(fp, p_classes_indices, updates)
+        tp_decisions = tf.cast(decisions, dtype=tf.int32)
+        tp = tf.tensor_scatter_nd_add(tp, p_classes_indices, tp_decisions)
+        fp_decisions = tf.cast(tf.math.logical_not(decisions), dtype=tf.int32)
+        fp = tf.tensor_scatter_nd_add(fp, p_classes_indices, fp_decisions)
 
         # t_unassigned, t_classes_indices)
-        updates = tf.cast(t_unassigned, dtype=tf.int32)
-        indices = tf.expand_dims(t_classes_indices, axis=-1)
+        fn_decisions = tf.cast(t_unassigned, dtype=tf.int32)
         try:
-            fn = tf.tensor_scatter_nd_add(fn, indices, updates)
+            fn = tf.tensor_scatter_nd_add(fn, tf.expand_dims(t_classes_indices, axis=-1), fn_decisions)
         except Exception as e:
             print(f'!!!Exception!!:  {e} probably negative  class id in dataset!! Skipping to next data sample!!') # todo check where -1 class come from
             errors = tf.math.add(errors, 1)
-            return tp, fp, fn, all, errors
+            return tp, fp, fn, true_obj, errors
 
         updates = tf.ones(tf.size(t_classes_indices), dtype=tf.int32)
         indices = tf.expand_dims(t_classes_indices, axis=-1)
-        all = tf.tensor_scatter_nd_add(all, indices, updates)
-        return tp, fp, fn, all, errors
+        true_obj = tf.tensor_scatter_nd_add(true_obj, indices, updates)
+        return tp, fp, fn, true_obj, errors
 
 
 
@@ -190,33 +186,37 @@ class Evaluate:
         tp = tf.convert_to_tensor([0] * nclasses)
         fp = tf.convert_to_tensor([0] * nclasses)
         fn = tf.convert_to_tensor([0] * nclasses)
-        all = tf.convert_to_tensor([0] * nclasses)
+        true_obj = tf.convert_to_tensor([0] * nclasses)
         errors = tf.Variable(0)
+        images = tf.Variable(0)
+
+        print(f'Stats Table:\n #image, accum tp per class, accum fp per class, accum fn per calss,  accum true objects per class, accum errors count')
 
         for idx, (pred_y, true_y) in enumerate(data):
             pred_y =tf.squeeze(pred_y, axis=0)
             t_unassigned = tf.Variable(tf.fill(tf.shape(true_y[..., 1]), 1))
 
-            tp, fp, fn, all, errors = tf.cond(tf.shape(true_y)[0] != 0,
-                                                 true_fn=lambda: self.do_iou(pred_y, true_y, tp, fp, fn, all, t_unassigned, errors),
+            tp, fp, fn, true, errors = tf.cond(tf.shape(true_y)[0] != 0,
+                                                 true_fn=lambda: self.do_iou(pred_y, true_y, tp, fp, fn, true_obj, t_unassigned, errors),
                                                   false_fn=lambda: (tp, fp,
                                                   tf.tensor_scatter_nd_add(fn,
                                                                            tf.expand_dims(tf.cast(true_y[..., 4], tf.int32), axis=-1),
                                                                            tf.fill(tf.shape(true_y)[0], 1)),
 
-                                                  tf.tensor_scatter_nd_add(all,
+                                                  tf.tensor_scatter_nd_add(true_obj,
                                                                            tf.expand_dims(
                                                                                tf.cast(true_y[..., 4], tf.int32),
                                                                                axis=-1),
                                                                            tf.fill(tf.shape(true_y)[0], 1)),errors
                                                                     )
                                                 )# rone todo fix inc_arg
+            images = tf.math.add(images, 1)
 
-            print(tp.numpy(), fp.numpy(), fn.numpy(), all.numpy(), errors.numpy())
+            print(images.numpy(), tp.numpy(), fp.numpy(), fn.numpy(), true_obj.numpy(), errors.numpy())
 
-        tn = all - tp -fp - fn
+        tn = true_obj - tp -fp - fn
 
-        accuracy = tf.cast(tp+tn, tf.float32)/(tf.cast(all, tf.float32) + 1e-10)
+        accuracy = tf.cast(tp+tn, tf.float32)/(tf.cast(true_obj, tf.float32) + 1e-10)
         recall = tf.cast(tp, tf.float32) / (tf.cast(tp + fn, tf.float32)  + 1e-20)
         precision = tf.cast(tp, tf.float32) / (tf.cast(tp + fp, tf.float32)  + 1e-20)
         print(accuracy, recall, precision)
